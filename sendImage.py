899309562f70f0de84d49a91c8ecf98519f244be
Chris Gregg <tofergregg@gmail.com>
2017-01-03 20:52:32 -0800

    updated sendImage.py for run-length encoding

--- sendImage.py
+++ sendImage.py
@@ -28,19 +28,45 @@ for idx,byte in enumerate(b):
 im2 = Image.frombytes('RGB',(im.width,im.height),b2)
 im2.show()
 
-bits = []
+# convert to run-length encoded stream
+# Format for bytes: (number of bits low byte)(number of bits high byte)(char to print)
+# char to print will normally be a period (.) or a space ( ) but will eventually
+# include an underscore (_) for faster printing
+
+runs = []
 for r in range(0,im2.height*3,3):
+    prevBit = ord(b2[r * im2.width])
+    bitCount = 0
     for c in range(0,im2.width*3,3):
+        currentBit = ord(b2[r * im2.width + c])
+        if currentBit != prevBit:
+            #sys.stdout.write(str(bitCount))
+            if prevBit == 0:
+                #sys.stdout.write('.')
+                runs.append((bitCount & 0xff, bitCount >> 8, '.'))
+            else:
+                #sys.stdout.write('x')
+                runs.append((bitCount & 0xff, bitCount >> 8,' '))
+            bitCount = 0
+            prevBit = currentBit
+        bitCount+=1
         #print(ord(b2[r * im2.width + c]))
         #print(r,c)
-        if (ord(b2[r * im2.width + c]) == 0):
-            sys.stdout.write('.')
-            bits.append(chr(1));
-        else:
-            sys.stdout.write(' ')
-            bits.append(chr(0))
-    sys.stdout.write('\n')
+        #if ord(b2[r * im2.width + c]) == 0:
+        #    sys.stdout.write('.')
+        #    bits.append(chr(1));
+        #else:
+        #    sys.stdout.write(' ')
+        #    bits.append(chr(0))
+    if prevBit == 0: # 0 means black
+        # don't bother printing a string of spaces at the end, just 1s
+        #sys.stdout.write(str(bitCount)+'.')
+        runs.append((bitCount & 0xff, bitCount >> 8,'.'))
+    #sys.stdout.write('\n')
+    runs.append((1,0,'\n'))
 
+print runs
+quit()
 ser = serial.Serial('/dev/cu.LightBlue-Bean', 57600, timeout=0.1)
 # wait a bit
 time.sleep(0.5)
